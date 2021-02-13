80b5456f52a35daf48920dd96a5265f74bfcb45c
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2021-02-13 14:06:02 +0200

    Add Create Listing Validation.

--- src/listings/dto/create-listing.dto.ts
+++ src/listings/dto/create-listing.dto.ts
@@ -1,9 +1,13 @@
 import { ApiModelProperty } from '@nestjs/swagger';
+import { IsString, IsOptional } from 'class-validator';
 
 export class CreateListingDto {
+  @IsString()
   @ApiModelProperty({ description: 'Title of the listing', required: true })
   readonly name: string;
 
+  @IsString()
+  @IsOptional()
   @ApiModelProperty({ description: 'Description and more information', required: false })
   readonly description?: string;
 }
